b0df49e00a11785c3e46c448dd26094bc65b580a
leonid.dashko <leonidda@ext.inditex.com>
2025-06-21 16:18:32 +0300

    extract listing parsing into separate method in "KvEeParser"

--- parsers/kvee_parser.py
+++ parsers/kvee_parser.py
@@ -53,65 +53,68 @@ class KvEeParser:
         articles = soup.find_all('article', attrs={'data-object-id': True})
 
         results = []
-
         for art in articles:
-            obj_id = art['data-object-id']
-            address = None
-            link = None
-            h2 = art.find('h2')
-            if h2:
-                for a in h2.find_all('a'):
-                    if 'object-promoted' not in (a.get('class') or []):
-                        address = a.get_text(strip=True)
-                        link = a.get('href')
-                        break
-
-            if link and link.startswith('/'):
-                link = KVEE_BASE_URL + link
-
-            price_tag = art.find("div", attrs={"data-price": True})
-            price = price_tag['data-price'] if price_tag else None
-
-            first_img = art.select_one("div.images img")
-            first_img_url = self.extract_img_url(first_img)
-
-            area_div = art.find('div', class_='area')
-            area_m2 = area_div.get_text(strip=True).replace('\u00a0m\u00b2', '') if area_div else None
-            price_m2 = int(float(price) / float(area_m2)) if price and area_m2 else None
-
-            object_important_note_p = art.find('p', class_='object-important-note')
-            object_important_note = object_important_note_p.get_text(strip=True) if object_important_note_p else None
-
-            desc_p = art.find('p', class_='object-excerpt')
-            description = desc_p.get_text(strip=True) if desc_p else None
-
-            floor = self.parse_floor(description)
-            year_built = self.parse_year_built(description)
-
-            rooms_div = art.find('div', class_='rooms')
-            rooms = rooms_div.get_text(strip=True) if rooms_div else None
-
-            extra_data = object_data_by_id_map.get(obj_id, {})
-            date_activated = extra_data.get('date_activated')
-            advertisement_level = extra_data.get('advertisment_level')
-
-            results.append(KvEeListing(
-                id=obj_id,
-                address=address,
-                rooms=rooms,
-                area_m2=area_m2,
-                price=price,
-                price_m2=str(price_m2) if price_m2 is not None else None,
-                link=link,
-                first_img_url=first_img_url,
-                object_important_note=object_important_note,
-                description=description,
-                date_activated=date_activated,
-                advertisement_level=advertisement_level,
-                floor=floor,
-                year_built=year_built
-            ))
+            listing = self.parse_listing(art, object_data_by_id_map)
+            results.append(listing)
         return results
+
+    def parse_listing(self, art, object_data_by_id_map: dict) -> KvEeListing:
+        obj_id = art['data-object-id']
+        address = None
+        link = None
+        h2 = art.find('h2')
+        if h2:
+            for a in h2.find_all('a'):
+                if 'object-promoted' not in (a.get('class') or []):
+                    address = a.get_text(strip=True)
+                    link = a.get('href')
+                    break
+
+        if link and link.startswith('/'):
+            link = KVEE_BASE_URL + link
+
+        price_tag = art.find("div", attrs={"data-price": True})
+        price = price_tag['data-price'] if price_tag else None
+
+        first_img = art.select_one("div.images img")
+        first_img_url = self.extract_img_url(first_img)
+
+        area_div = art.find('div', class_='area')
+        area_m2 = area_div.get_text(strip=True).replace('\u00a0m\u00b2', '') if area_div else None
+        price_m2 = int(float(price) / float(area_m2)) if price and area_m2 else None
+
+        object_important_note_p = art.find('p', class_='object-important-note')
+        object_important_note = object_important_note_p.get_text(strip=True) if object_important_note_p else None
+
+        desc_p = art.find('p', class_='object-excerpt')
+        description = desc_p.get_text(strip=True) if desc_p else None
+
+        floor = self.parse_floor(description)
+        year_built = self.parse_year_built(description)
+
+        rooms_div = art.find('div', class_='rooms')
+        rooms = rooms_div.get_text(strip=True) if rooms_div else None
+
+        extra_data = object_data_by_id_map.get(obj_id, {})
+        date_activated = extra_data.get('date_activated')
+        advertisement_level = extra_data.get('advertisment_level')
+
+        return KvEeListing(
+            id=obj_id,
+            address=address,
+            rooms=rooms,
+            area_m2=area_m2,
+            price=price,
+            price_m2=str(price_m2) if price_m2 is not None else None,
+            link=link,
+            first_img_url=first_img_url,
+            object_important_note=object_important_note,
+            description=description,
+            date_activated=date_activated,
+            advertisement_level=advertisement_level,
+            floor=floor,
+            year_built=year_built
+        )
 
     def extract_img_url(self, img_el):
         if not img_el:
